a4cb13a0262f349d334e5853d1a815ddf6a69240
Jacob Gonzalez <njacob1001@gmail.com>
2020-06-22 22:35:09 -0500

    action bar

---
+++ app/src/main/java/com/jacoblabs/project/cotactos/Detalle.kt
@@ -0,0 +1,11 @@
+package com.jacoblabs.project.cotactos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detalle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle)
+    }
+}
---
+++ app/src/main/java/com/jacoblabs/project/cotactos/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jacoblabs.project.cotactos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
